9a645f5052e1c08bcd533587ce6eaa76ccd009f2
Davi J <davijosantos.dj@gmail.com>
2024-02-17 11:37:57 -0300

    Iniciando projeto e configurando libs e arquivos

---
+++ src/styles/global.ts
@@ -0,0 +1,24 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+  * {
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+  }
+
+  :focus {
+    outline: none;
+    box-shadow: ${props => props.theme["green-500"]};
+  }
+
+  body {
+    background: ${props => props.theme["gray-800"]};
+    color: ${props => props.theme["gray-100"]};
+    -webkit-font-smothing: antialiased;
+  }
+
+  body, input, button, textarea {
+    font: 400 1rem Roboto, sans-serif;
+  }
+`
