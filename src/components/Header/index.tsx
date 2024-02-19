cd71b04260d1cea0cce5f069ac29a3f33af61bfc
Davi J <davijosantos.dj@gmail.com>
2024-02-19 16:09:11 -0300

    Criando um modal acessível

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,30 @@
 import { HeaderContainer, HeaderContent, NewTransactionButtton } from "./styles";
-import LogoImg from '../../assets/Logo.svg'
+import * as Dialog from '@radix-ui/react-dialog';
+import LogoImg from '../../assets/Logo.svg';
 
 export function Header() {
   return (
     <HeaderContainer>
       <HeaderContent>
         <img src={LogoImg} />
-        <NewTransactionButtton>
-          Nova transação
-        </NewTransactionButtton>
+
+        <Dialog.Root>
+          <Dialog.Trigger asChild>
+            <NewTransactionButtton>
+              Nova transação
+            </NewTransactionButtton>
+          </Dialog.Trigger>
+
+          <Dialog.Portal>
+            <Dialog.Overlay/>
+            <Dialog.Content>
+             <Dialog.Title>
+              Nova Transação
+             </Dialog.Title>
+             <Dialog.Close/>
+            </Dialog.Content>          
+          </Dialog.Portal>
+        </Dialog.Root>
       </HeaderContent>
     </HeaderContainer>
   )
--- src/components/SearchForm/index.tsx
+++ src/components/SearchForm/index.tsx
@@ -4,7 +4,7 @@ import { SearchFormContainer } from "./styles";
 export function SearchForm() {
   return (
     <SearchFormContainer>
-      <input type="text" placeholder="Busque uma transação" />
+      <input type="text" placeholder="Busque por transações" />
       <button type="submit">
         <MagnifyingGlass size={20}/>
         Buscar
